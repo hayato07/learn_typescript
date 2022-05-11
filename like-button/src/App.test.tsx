c3878f5c2389690d6e9dd347624f3ea02c023443
Hayato Ando <0du38604360418k@gmail.com>
2022-05-11 20:27:36 +0900

    最終コードに合わせたテストを作成

--- like-button/src/App.test.tsx
+++ like-button/src/App.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn the react/i);
-  expect(linkElement).toBeInTheDocument();
+  const LikeButtonElement = screen.getByText(/999/i);
+  expect(LikeButtonElement).toBeInTheDocument();
+  userEvent.click(LikeButtonElement);
+  expect(LikeButtonElement.textContent).toBe('♥ 1000');
 });
