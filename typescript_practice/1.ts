66f6f2133a01113ee4ca3f008976c882413718b0
Hayato Ando <0du38604360418k@gmail.com>
2022-05-13 19:26:07 +0900

    2-4まで

--- typescript_practice/1.ts
+++ typescript_practice/1.ts
@@ -77,3 +77,113 @@ function sumOfPos(arr: Array<number>):number {
   // エラー例
 //   sumOfPos(123, 456);
 //   sumOfPos([123, "foobar"]);
+
+// 2-1 ジェネリクス 
+
+function myFilter<T>(arr: T[], predicate: (elm:T) => boolean) {
+    const result = [];
+    for (const elm of arr) {
+      if (predicate(elm)) {
+        result.push(elm);
+      }
+    }
+    return result;
+  }
+  
+  // 使用例
+  const res = myFilter([1, 2, 3, 4, 5], num => num % 2 === 0);
+  const res2 = myFilter(['foo', 'hoge', 'bar'], str => str.length >= 4);
+  
+  // エラー例
+//   myFilter([1, 2, 3, 4, 5], str => str.length >= 4);
+
+// 2-2 OK ユニオン型便利そう！！
+
+type Speed = 'slow' | 'medium' | 'fast';
+
+function getSpeed(speed: Speed): number {
+  switch (speed) {
+    case "slow":
+      return 10;
+    case "medium":
+      return 50;
+    case "fast":
+      return 200;
+  }
+}
+
+// 使用例
+const slowSpeed = getSpeed("slow");
+const mediumSpeed = getSpeed("medium");
+const fastSpeed = getSpeed("fast");
+
+// エラー例
+// getSpeed("veryfast");
+
+// 2-3 NOT GOOD 全然わからんかった
+
+// 自分の回答
+// declare var addEventListener:(s:string, func:object, bool:boolean?);
+
+interface AddEventListnerOptionsObject {
+    capture?: boolean;
+    once?: boolean;
+    passive?: boolean;
+}
+
+// declareはTypeScriptに特有の構文であり、関数や変数の型を中身なしに宣言できる構文です。
+declare function addEventListener(
+    type: string,
+    handler: () => {},
+    options?: boolean | AddEventListnerOptionsObject
+)
+
+// 使用例
+
+addEventListener("foobar", () => {});
+addEventListener("event", () => {}, true);
+addEventListener("event2", () => {}, {});
+addEventListener("event3", () => {}, {
+  capture: true,
+  once: false
+});
+
+// エラー例
+// addEventListener("foobar", () => {}, "string");
+// addEventListener("hoge", () => {}, {
+//   capture: true,
+//   once: false,
+//   excess: true
+// });
+
+// 2-4 なんとなく理解が進んでいる<T>が結構便利な印象だけど、使いみちを考えてやらないといけないかも
+
+function giveId<T>(obj:T): T & {id: string} {
+    const id = "本当はランダムがいいけどここではただの文字列";
+    return {
+      ...obj,
+      id
+    };
+  }
+  
+  // 使用例
+  const obj1: {
+    id: string;
+    foo: number;
+  } = giveId({ foo: 123 });
+  const obj2: {
+    id: string;
+    num: number;
+    hoge: boolean;
+  } = giveId({
+    num: 0,
+    hoge: true
+  });
+  
+  // エラー例
+//   const obj3: {
+//     id: string;
+//     piyo: string;
+//   } = giveId({
+//     foo: "bar"
+//   });
