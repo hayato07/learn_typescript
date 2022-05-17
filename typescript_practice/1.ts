ea1176d27ce0f1334c11701822c3bffe2c72d854
Hayato Ando <0du38604360418k@gmail.com>
2022-05-17 21:09:26 +0900

    4-1まで

--- typescript_practice/1.ts
+++ typescript_practice/1.ts
@@ -4,7 +4,7 @@
  */
 
 // 1-1 OK
-function isPositive1(num: number):boolean {
+function isPositive1(num: number): boolean {
     return num >= 0;
 }
 
@@ -17,8 +17,8 @@ isPositive1(3);
 
 // 1-2 Good (typeとinterfaceの使い分けがいまいち)
 type User = {
-    name : string,
-    age : number,
+    name: string,
+    age: number,
     private: boolean
 };
 
@@ -67,34 +67,34 @@ isPositive2(5);
 // 1-4 OK
 // こちらでもOK
 // function sumOfPos(arr: number[]):number {
-function sumOfPos(arr: Array<number>):number {
+function sumOfPos(arr: Array<number>): number {
     return arr.filter(num => num >= 0).reduce((acc, num) => acc + num, 0);
 }
-  
-  // 使用例
-  const sum: number = sumOfPos([1, 3, -2, 0]);
-  
-  // エラー例
+
+// 使用例
+const sum: number = sumOfPos([1, 3, -2, 0]);
+
+// エラー例
 //   sumOfPos(123, 456);
 //   sumOfPos([123, "foobar"]);
 
 // 2-1 ジェネリクス 
 
-function myFilter<T>(arr: T[], predicate: (elm:T) => boolean) {
+function myFilter<T>(arr: T[], predicate: (elm: T) => boolean) {
     const result = [];
     for (const elm of arr) {
-      if (predicate(elm)) {
-        result.push(elm);
-      }
+        if (predicate(elm)) {
+            result.push(elm);
+        }
     }
     return result;
-  }
-  
-  // 使用例
-  const res = myFilter([1, 2, 3, 4, 5], num => num % 2 === 0);
-  const res2 = myFilter(['foo', 'hoge', 'bar'], str => str.length >= 4);
-  
-  // エラー例
+}
+
+// 使用例
+const res = myFilter([1, 2, 3, 4, 5], num => num % 2 === 0);
+const res2 = myFilter(['foo', 'hoge', 'bar'], str => str.length >= 4);
+
+// エラー例
 //   myFilter([1, 2, 3, 4, 5], str => str.length >= 4);
 
 // 2-2 OK ユニオン型便利そう！！
@@ -102,14 +102,14 @@ function myFilter<T>(arr: T[], predicate: (elm:T) => boolean) {
 type Speed = 'slow' | 'medium' | 'fast';
 
 function getSpeed(speed: Speed): number {
-  switch (speed) {
-    case "slow":
-      return 10;
-    case "medium":
-      return 50;
-    case "fast":
-      return 200;
-  }
+    switch (speed) {
+        case "slow":
+            return 10;
+        case "medium":
+            return 50;
+        case "fast":
+            return 200;
+    }
 }
 
 // 使用例
@@ -140,12 +140,12 @@ declare function addEventListener(
 
 // 使用例
 
-addEventListener("foobar", () => {});
-addEventListener("event", () => {}, true);
-addEventListener("event2", () => {}, {});
-addEventListener("event3", () => {}, {
-  capture: true,
-  once: false
+addEventListener("foobar", () => { });
+addEventListener("event", () => { }, true);
+addEventListener("event2", () => { }, {});
+addEventListener("event3", () => { }, {
+    capture: true,
+    once: false
 });
 
 // エラー例
@@ -158,29 +158,29 @@ addEventListener("event3", () => {}, {
 
 // 2-4 なんとなく理解が進んでいる<T>が結構便利な印象だけど、使いみちを考えてやらないといけないかも
 
-function giveId<T>(obj:T): T & {id: string} {
+function giveId<T>(obj: T): T & { id: string } {
     const id = "本当はランダムがいいけどここではただの文字列";
     return {
-      ...obj,
-      id
+        ...obj,
+        id
     };
-  }
-  
-  // 使用例
-  const obj1: {
+}
+
+// 使用例
+const obj1: {
     id: string;
     foo: number;
-  } = giveId({ foo: 123 });
-  const obj2: {
+} = giveId({ foo: 123 });
+const obj2: {
     id: string;
     num: number;
     hoge: boolean;
-  } = giveId({
+} = giveId({
     num: 0,
     hoge: true
-  });
-  
-  // エラー例
+});
+
+// エラー例
 //   const obj3: {
 //     id: string;
 //     piyo: string;
@@ -212,3 +212,198 @@ setAnotherState(100);
 // 静的解析かしこ！！
 // setNumState('foobar');
 
+// 3-1 extends keyofもなかなか便利そう
+
+// function mapFromArray<T, K extends keyof T>(arr: T[], key: K): Map<T[K], T> {
+//     const result = new Map();
+//     for (const obj of arr) {
+//         result.set(obj[key], obj);
+//     }
+
+//     return result;
+// }
+
+// 使用例
+// const data = [
+//     { id: 1, name: "John Smith" },
+//     { id: 2, name: "Mary Sue" },
+//     { id: 100, name: "Taro Yamada" }
+// ];
+// const dataMap = mapFromArray(data, "id");
+/*
+dataMapは
+Map {
+  1 => { id: 1, name: 'John Smith' },
+  2 => { id: 2, name: 'Mary Sue' },
+  100 => { id: 100, name: 'Taro Yamada' }
+}
+というMapになる
+*/
+
+// エラー例
+//   mapFromArray(data, "age");
+
+// 3-2
+
+type MyPartial<T> = {
+    [K in keyof T]?: T[K]
+};
+
+// 使用例
+/*
+ * T1は { foo?: number; bar?: string; } となる
+ */
+type T1 = MyPartial<{
+    foo: number;
+    bar: string;
+}>;
+/*
+ * T2は { hoge?: { piyo: number; } } となる
+ */
+type T2 = MyPartial<{
+    hoge: {
+        piyo: number;
+    };
+}>;
+
+// 3-3
+
+interface EventPayloads {
+    start: {
+        user: string;
+    };
+    stop: {
+        user: string;
+        after: number;
+    };
+    end: {};
+}
+
+class EventDischarger<E> {
+    emit<Ev extends keyof E>(eventName: Ev, payload: E[Ev]) {
+        // 省略
+    }
+}
+
+// 使用例
+const ed = new EventDischarger<EventPayloads>();
+ed.emit("start", {
+    user: "user1"
+});
+ed.emit("stop", {
+    user: "user1",
+    after: 3
+});
+ed.emit("end", {});
+
+// エラー例
+// ed.emit("start", {
+//     user: "user2",
+//     after: 0
+// });
+// ed.emit("stop", {
+//     user: "user2"
+// });
+// ed.emit("foobar", {
+//     foo: 123
+// });
+
+// 3-4
+
+type Action = 
+    | {
+        type: "increment";
+        amount: number;
+    }
+    | {
+        type: "decrement";
+        amount: number;
+    }
+    | {
+        type: "reset";
+        value: number;
+    };
+
+const reducer = (state: number, action: Action) => {
+    switch (action.type) {
+      case "increment":
+        return state + action.amount;
+      case "decrement":
+        return state - action.amount;
+      case "reset":
+        return action.value;
+    }
+  };
+  
+  // 使用例
+  reducer(100, {
+      type: 'increment',
+      amount: 10,
+  }) === 110;
+  reducer(100, {
+      type: 'decrement',
+      amount: 55,
+  }) === 45;
+  reducer(500, {
+      type: 'reset',
+      value: 0,
+  }) === 0;
+  
+  // エラー例
+//   reducer(0,{
+//       type: 'increment',
+//       value: 100,
+//   });
+
+
+// 3-5
+
+// type Func<A, R> = (arg: A) => R;
+type Func<A, R> = undefined extends A ? (arg?: A) => R : (arg: A) => R;
+
+
+// 使用例
+const f1: Func<number, number> = num => num + 10;
+const v1: number = f1(10);
+
+const f2: Func<undefined, number> = () => 0;
+const v2: number = f2();
+const v3: number = f2(undefined);
+
+const f3: Func<number | undefined, number> = num => (num || 0) + 10;
+const v4: number = f3(123);
+const v5: number = f3();
+
+// エラー例 なぜかうごく。。。
+const v6: number = f1();
+
+
+// 4-1
+// extendsでobjectという制約をつける
+// これが、 conditional type => T extends { foo: infer E }
+// Tがfooプロパティを持つならば、そのプロパティの型をEとして取得する
+function getFoo<T extends object>(
+        obj: T
+    ): T extends { foo: infer E } ? E : unknown {
+        // return obj.foo;
+        return (obj as any).foo;
+  }
+  
+  // 使用例
+  // numはnumber型
+  const num = getFoo({
+    foo: 123
+  });
+  // strはstring型
+  const str = getFoo({
+    foo: "hoge",
+    bar: 0
+  });
+  // unkはunknown型
+  const unk = getFoo({
+    hoge: true
+  });
+  
+  // エラー例
+//   getFoo(123);
+  getFoo(null); // こっちはエラーにならない。。
