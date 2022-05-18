30c3af42d211c749c5546c6afb9f777394d0a4d9
Hayato Ando <0du38604360418k@gmail.com>
2022-05-18 21:25:12 +0900

    improve tutorial

--- react_tutorial/src/index.js
+++ react_tutorial/src/index.js
@@ -19,24 +19,24 @@ class Board extends React.Component {
     }
 
     render() {
+        const winner_line = getWinnerLine(this.props.squares);
+        let boardSquares = [];
+        for (let row = 0; row < 3; row++) {
+            let boardRow = [];
+            for (let col = 0; col < 3; col++) {
+                const squareNumber = (row * 3) + col;
+                const className = (winner_line && winner_line.includes(squareNumber))
+                ? 'win_square'
+                : '';
+
+                boardRow.push(<span className={className} key={squareNumber}>{this.renderSquare(squareNumber)}</span>);
+            }
+            boardSquares.push(<div className='board-row' key={row}>{boardRow}</div>);
+        };
 
         return (
             <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+                {boardSquares}
             </div>
         );
     }
@@ -49,6 +49,7 @@ class Game extends React.Component {
             history: [{
                 squares: Array(9).fill(null),
             }],
+            IsHistoryAsc: true,
             stepNumber: 0,
             xIsNext: true,
         };
@@ -59,7 +60,7 @@ class Game extends React.Component {
         const current = history[history.length - 1];
         const squares = current.squares.slice();
 
-        if(calculateWinner(squares) || squares[i]) {
+        if (calculateWinner(squares) || squares[i]) {
             return;
         }
 
@@ -74,6 +75,12 @@ class Game extends React.Component {
         })
     }
 
+    handleSortClick() {
+        this.setState({
+            IsHistoryAsc: !this.state.IsHistoryAsc,
+        })
+    }
+
     jumpTo(step) {
         this.setState({
             stepNumber: step,
@@ -85,33 +92,55 @@ class Game extends React.Component {
         const history = this.state.history;
         const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
-        const status = (winner) 
-                        ? 'Winner: ' + winner
-                        : 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        const isDraw = history.length === 10;
+        const status = (winner)
+            ? 'Winner: ' + winner
+            : (isDraw) 
+            ? 'Draw'
+            :'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
-        const moves = history.map((step, move) => {
+        let moves = history.map((step, move) => {
+            let move_index;
+            if (move) {
+                history[move].squares.forEach((value, index) => {
+                    if (history[move - 1].squares[index] !== value) {
+                        move_index = index;
+                    }
+                })
+            }
+
             const desc = move ?
-                'Go to move #' + move:
+                'Go to move #' + move + " 列: " + (move_index % 3 + 1) + " 行:" + (Math.floor(move_index / 3) + 1) :
                 'Go to game start';
+            const className = (move === this.state.stepNumber) ? "current" : "";
+
             return (
-                <li key={move}>
+                <li className={className} key={move}>
                     <button onClick={() => this.jumpTo(move)}>
                         {desc}
                     </button>
                 </li>
             );
         });
+
+        moves = this.state.IsHistoryAsc
+            ? moves
+            : moves.reverse();
+
+        const sort_status = this.state.IsHistoryAsc ? "昇順": "逆順";
+        
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board 
+                    <Board
                         squares={current.squares}
                         onClick={(i) => this.handleClick(i)}
                     />
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                    <ol>{moves}</ol>
+                    <button onClick={() => this.handleSortClick()}>sort: {sort_status}</button>
+                    <ul className='game-history'>{moves}</ul>
                 </div>
             </div>
         );
@@ -133,8 +162,31 @@ function calculateWinner(squares) {
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
 
-        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
             return squares[a];
+        }
+    }
+
+    return null;
+}
+
+function getWinnerLine(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return [a, b, c];
         }
     }
 
