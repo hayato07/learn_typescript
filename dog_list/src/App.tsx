592698def0873c650c8b4439106b0aec1954aa2f
Hayato Ando <0du38604360418k@gmail.com>
2022-06-14 20:49:04 +0900

    dog-list: 静的犬画像表示

--- dog_list/src/App.tsx
+++ dog_list/src/App.tsx
@@ -1,7 +1,108 @@
+function Header() {
+    return (
+        <header className="hero is-dark is-bold">
+            <div className="hero-body">
+                <div className="container">
+                    <h1 className="title">Cute Dog Images</h1>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+type ImageType = {
+    src: string | undefined;
+    alt: string | undefined;
+}
+
+function Image(props: ImageType) {
+    return (
+        <div className="card">
+            <div className="card-image">
+                <figure className="image">
+                    <img src={props.src} alt={props.alt} />
+                </figure>
+            </div>
+        </div>
+    );
+}
+
+function Loading() {
+    return <p>Loading...</p>;
+}
+
+type GalleryType = {
+    urls: string[];
+}
+
+function Gallery(props: GalleryType) {
+    const { urls } = props;
+    const alt = "cute dog";
+
+    if(urls == null) {
+        return <Loading />;
+    }
+
+    return (
+        <div className="columns is-vcenterd is-multiline">
+            {
+                urls.map((url) => {
+                    return (
+                        <div key={url} className="column is-3">
+                            <Image src={url} alt={alt} />
+                        </div>
+                    )
+                })
+            }
+        </div>
+    );
+}
+
+function Main() {
+    const urls = [
+        "https://images.dog.ceo/breeds/shiba/shiba-11.jpg",
+        "https://images.dog.ceo/breeds/shiba/shiba-12.jpg",
+        "https://images.dog.ceo/breeds/shiba/shiba-14.jpg",
+        "https://images.dog.ceo/breeds/shiba/shiba-17.jpg",
+        "https://images.dog.ceo/breeds/shiba/shiba-2.jpg",
+        "https://images.dog.ceo/breeds/shiba/shiba-3i.jpg",
+        "https://images.dog.ceo/breeds/shiba/shiba-4.jpg",
+        "https://images.dog.ceo/breeds/shiba/shiba-5.jpg",
+        "https://images.dog.ceo/breeds/shiba/shiba-6.jpg",
+        "https://images.dog.ceo/breeds/shiba/shiba-7.jpg",
+        "https://images.dog.ceo/breeds/shiba/shiba-8.jpg",
+        "https://images.dog.ceo/breeds/shiba/shiba-9.jpg",
+    ];
+
+    
+    return (
+        <main>
+            <section className="section">
+                <div className="container">
+                    <Gallery urls={urls} />
+                </div>
+            </section>
+        </main>
+    )
+}
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <p>Dog images are retrieved from Dog API</p>
+            <p>
+                <a href="https://dog.ceo/dog-api/about">Donate to Dog API</a>
+            </p>
+        </footer>
+    );
+}
+
 function App() {
     return (
         <div>
-            <h1>Hello, World!</h1>
+            <Header />
+            <Main />
+            <Footer />
         </div>
     )
 }
--- dog_list/src/main.tsx
+++ dog_list/src/main.tsx
@@ -1,3 +1,4 @@
+import "bulma/css/bulma.css";
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
